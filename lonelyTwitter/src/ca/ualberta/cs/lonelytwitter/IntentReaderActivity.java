929cab4e4ad78f72aa2f0c533afdae49067feb95
sbaergen <sbaergen@ualberta.ca>
2015-02-11 15:59:00 -0700

    Added new tests

--- lonelyTwitter/src/ca/ualberta/cs/lonelytwitter/IntentReaderActivity.java
+++ lonelyTwitter/src/ca/ualberta/cs/lonelytwitter/IntentReaderActivity.java
@@ -13,32 +13,42 @@ public class IntentReaderActivity extends Activity {
 	public static final int NORMAL = 1;
 	public static final int REVERSE = 2;
 	public static final int DOUBLE = 3;
-	
+
 	private String text;
 	private int mode;
-	
+
 	public String getText() {
 		return text;
 	}
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_intent_reader);
+		Intent intent = getIntent();
+		text = intent.getStringExtra(TEXT_KEY);
+		mode = intent.getIntExtra(TRANSFORM_KEY, NORMAL);
+		text = transformText(text);
+		TextView view = (TextView) findViewById(R.id.intentText);
+		if (text == null) {
+			view.setText("No Message");
+		} else {
+			view.setText(text);
+		}
 	}
-	
+
 	public String transformText(String text) {
 		switch (mode) {
-			case REVERSE:
-				char[] string =  text.toCharArray();
-				for (int i = 0; i < string.length / 2; i++) {
-					char tmp = string[i];
-					string[i] = string[string.length - i -1];
-					string[string.length - i - 1] = tmp;
-				}
-				return new String(string);
-			case DOUBLE:
-				return text + text;
+		case REVERSE:
+			char[] string = text.toCharArray();
+			for (int i = 0; i < string.length / 2; i++) {
+				char tmp = string[i];
+				string[i] = string[string.length - i - 1];
+				string[string.length - i - 1] = tmp;
+			}
+			return new String(string);
+		case DOUBLE:
+			return text + text;
 		}
 		return text;
 	}
--- lonelyTwitterTests/src/ca/ualberta/cs/lonelytwitter/test/LonelyTwitterActivityUITest.java
+++ lonelyTwitterTests/src/ca/ualberta/cs/lonelytwitter/test/LonelyTwitterActivityUITest.java
@@ -3,13 +3,13 @@ package ca.ualberta.cs.lonelytwitter.test;
 import android.app.Activity;
 import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
-import android.test.ViewAsserts;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity;
 import ca.ualberta.cs.lonelytwitter.NormalTweetModel;
+import ca.ualberta.cs.lonelytwitter.R;
 
 /*
  * generate this class with new.. JUnit Test Case
@@ -34,6 +34,44 @@ public class LonelyTwitterActivityUITest extends
 		textInput = ((EditText) activity.findViewById(ca.ualberta.cs.lonelytwitter.R.id.body));
 	}
 	
+	public void testSetText(){
+		String text = "NeatO!!";
+		instrumentation.runOnMainSync(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				String text = "NeatO!!";
+				textInput.setText(text);
+				
+			
+			}
+		});
+		instrumentation.waitForIdleSync();
+		assertEquals("DOES THIS WORK????", text, textInput.getText().toString());
+		}
+	
+	
+	public void testAddTweet(){
+		ListView list = (ListView) activity.findViewById(R.id.oldTweetsList);
+		ListAdapter adapter = list.getAdapter();
+		int length = adapter.getCount();
+		NormalTweetModel tweet = new NormalTweetModel();
+		tweet.setText("Tweet");
+		instrumentation.runOnMainSync(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				String text = "Tweet";
+				makeTweet(text);
+			}
+		});
+		instrumentation.waitForIdleSync();
+		assertEquals("Adapter", length+1, adapter.getCount());
+		assertEquals("Type", tweet.getClass(), adapter.getItem(adapter.getCount()-1).getClass());
+		assertEquals("Text", "Tweet", ((NormalTweetModel) adapter.getItem(adapter.getCount()-1)).getText());
+	}
 	/*
 	 * fills in the input text field and clicks the 'save'
 	 * button for the activity under test
